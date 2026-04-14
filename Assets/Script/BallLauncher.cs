c7571f75c24c5d49677d86ce4b60688bea4ccdc5
kaki05uni <zmyn8tc6yz@privaterelay.appleid.com>
2026-04-14 23:24:20 +0900

    Initial commit

---
+++ Assets/Script/BallLauncher.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class BallLauncher: MonoBehaviour
+{
+    public GameObject ballPrefab;
+    public Transform firePoint;
+    public float shotForce=15f;
+
+    [Header("角度調整")]
+    
+    [Range(-45f,45f)] 
+    public float launchAngle=-45;//-45でｙ軸正に45度
+    
+    [Range(-180f,180f)] 
+    public float horizontalAngle=90f;
+
+    void Update(){
+        transform.localRotation=Quaternion.Euler(launchAngle,horizontalAngle,0);
+
+        if(Keyboard.current!=null && Keyboard.current.enterKey.wasPressedThisFrame){
+            Launch();
+        }
+    }
+
+    void Launch(){
+        GameObject ball=Instantiate(ballPrefab,firePoint.position,firePoint.rotation);
+
+        Rigidbody rb=ball.GetComponent<Rigidbody>();
+        if(rb!=null){
+            rb.AddForce(firePoint.forward*shotForce,ForceMode.Impulse);
+        }
+    }
+}
